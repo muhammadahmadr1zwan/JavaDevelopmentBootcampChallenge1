6d0ed41a108c0d713ab23822190e2e99d1d956e4
Muhammad Rizwan <ahmadzainab@outlook.com>
2024-09-16 23:47:49 -0400

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         System.out.println("  A    RRRR");
+         System.out.println(" A A   R    R");
+         System.out.println("A   A  R    R");
+         System.out.println("AAAAA  RRRR");
+         System.out.println("A   A  R R");
+         System.out.println("A   A  R  R");
+         System.out.println("A   A  R    R");
+
+    }
+}
